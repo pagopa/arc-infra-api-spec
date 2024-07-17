5aa448b983371554f4f2b073e6e6f6b64854e7ef
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2024-07-17 23:01:31 +0200

    update cors policy & terraform providers

--- src/cittadini/01_core.tf
+++ src/cittadini/01_core.tf
@@ -20,39 +20,8 @@ module "apim_cittadini_product" {
   approval_required     = false
   subscriptions_limit   = 0
 
-  policy_xml = file("./api_product/cittadini_base_policy.xml")
-}
-
-resource "azurerm_api_management_api_version_set" "cittadini_ver_set" {
-
-  name                = "${var.env_short}-cittadini-version-set"
-  resource_group_name = local.apim_rg
-  api_management_name = local.apim_name
-  display_name        = "Cittadini API"
-  versioning_scheme   = "Segment"
-}
-
-module "apim_api_cittadini_v1" {
-  source = "./.terraform/modules/__v3__/api_management_api"
-
-  name                  = "${local.project}-cittadini-api"
-  api_management_name   = local.apim_name
-  resource_group_name   = local.apim_rg
-  product_ids           = [module.apim_cittadini_product.product_id]
-  subscription_required = false
-  version_set_id        = azurerm_api_management_api_version_set.cittadini_ver_set.id
-  api_version           = "v1"
-
-  description  = ""
-  display_name = "Cittadini API"
-  path         = ""
-  protocols    = ["https"]
-  service_url  = "https://${var.dns_zone_internal_entry}.${var.apim_dns_zone_prefix}.${var.external_domain}/pagopaarcbe/arc"
-
-  content_format = "openapi"
-  content_value = templatefile("./api/cittadini/v1/area_riservata_cittadino_openapi.yml", {
-    hostname = local.apim_hostname
+  policy_xml = templatefile("./api_product/_base_policy.xml", {
+    fe_origin  = local.fe_hostname
+    dev_origin = var.env_short != "d" ? "" : "\n<origin>https://arcdcittadinisa.privatelink.web.core.windows.net</origin>"
   })
-
-  xml_content = templatefile("./api/cittadini/v1/area_riservata_cittadino_base_policy.xml", {})
 }
